4fd47bafccd32acbb133002d18fecf68202aa9c8
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2021-11-26 15:52:53 +0100

    Minor corrections

--- test/tr_SUITE.erl
+++ test/tr_SUITE.erl
@@ -1,8 +1,7 @@
 -module(tr_SUITE).
--compile(export_all).
+-compile([export_all, nowarn_export_all]).
 
 -include_lib("eunit/include/eunit.hrl").
--include_lib("common_test/include/ct.hrl").
 -include("tr.hrl").
 
 %% CT callbacks
